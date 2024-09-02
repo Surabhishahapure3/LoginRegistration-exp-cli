7a9b773a575cc454b9dccca0aab2c89ecf373afa
Surabhishahapure3 <surabhishahapure3@gmail.com>
2024-09-02 11:28:42 +0530

    [surabhi]added:Modified middlewares file

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -25,9 +25,11 @@ export const userAuth = async (
       };
     bearerToken = bearerToken.split(' ')[1];
 
-    const { user }: any = await jwt.verify(bearerToken, process.env.JWT_SECRET);
-    res.locals.user = user;
-    res.locals.token = bearerToken;
+    const  user : any = await jwt.verify(bearerToken, process.env.JWT_SECRET);
+    console.log("token user",user)
+    req.body.createdby = user.id
+    // res.locals.user = user;
+    // res.locals.token = bearerToken;
     next();
   } catch (error) {
     next(error);
