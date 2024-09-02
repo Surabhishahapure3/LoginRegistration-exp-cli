a872dd667ce761d956a0e26cc9a9c66e286a9ac8
Surabhishahapure3 <surabhishahapure3@gmail.com>
2024-09-02 11:29:22 +0530

    [surabhi]added:Modified models file

---
+++ src/models/note.model.ts
@@ -0,0 +1,34 @@
+import { required } from '@hapi/joi';
+import { Schema, model } from 'mongoose';
+// import {INote} from '../interfaces/note.interface';
+
+const noteSchema = new Schema(
+    {
+        Title: {
+            type:String,    
+            required:true,
+        },
+        Description: {
+            type:String,
+            required:true,
+        },
+        color: {
+            type:String
+        },
+        isarchive:{
+            type:Boolean
+        },
+        isdeleted:
+        {
+            type:Boolean
+        },
+        createdby:{
+            type: String 
+        }
+    },
+    {
+        timestamps: true
+      }
+);
+
+export default model('Note',noteSchema);
